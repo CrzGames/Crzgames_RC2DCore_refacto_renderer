ba335794ca6d1b720475966f88b83e0932e922e6
Corentin Recanzone <corentindevpro@hotmail.com>
2025-09-13 04:53:55 +0200

    x

--- build-scripts/build-run-mobile/ios.sh
+++ build-scripts/build-run-mobile/ios.sh
@@ -6,7 +6,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-CONFIGURATION="Release"
+CONFIGURATION="Debug" # Default configuration
 
 # Process command line arguments
 while [[ "$#" -gt 0 ]]; do
