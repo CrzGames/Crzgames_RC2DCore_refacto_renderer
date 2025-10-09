8af162afe038f4cb3e3195891b4a205b9d77f2ba
Corentin Recanzone <corentindevpro@hotmail.com>
2025-10-09 05:03:59 +0200

    x

--- include/RC2D/RC2D_export.h
+++ include/RC2D/RC2D_export.h
@@ -4,15 +4,13 @@
 #include <RC2D/RC2D_platform_defines.h> // Required for : RC2D_PLATFORM_*
 
 #if defined(RC2D_PLATFORM_WINDOWS)
-  #if defined(RC2D_BUILDING_SHARED)
-    #define RC2D_DECLSPEC __declspec(dllexport)
-  #else
-    #define RC2D_DECLSPEC __declspec(dllimport)
-  #endif
-  #define RC2D_CALL __cdecl
+#if defined(RC2D_BUILDING_SHARED)
+#define RC2D_DECLSPEC __declspec(dllexport)
+#endif // RC2D_BUILDING_SHARED
+#define RC2D_CALL __cdecl
 #else
-  #define RC2D_DECLSPEC __attribute__((visibility("default")))
-  #define RC2D_CALL
-#endif
+#define RC2D_DECLSPEC __attribute__((visibility("default")))
+#define RC2D_CALL
+#endif // RC2D_PLATFORM_WINDOWS
 
 #endif // RC2D_EXPORT_H
