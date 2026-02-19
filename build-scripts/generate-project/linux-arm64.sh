78517e90ab53095787e407d95c1c37e31cdc69bd
Corentin Recanzone <corentindevpro@hotmail.com>
2026-02-20 00:42:08 +0100

    x

--- build-scripts/generate-project/linux-arm64.sh
+++ build-scripts/generate-project/linux-arm64.sh
@@ -4,11 +4,17 @@ echo -e "\e[32m\nGenerating Unix Makefiles project for Linux arm64...\e[0m"
 
 for build_type in Debug Release; do
   echo -e "\e[32m\nBuilding $build_type...\e[0m"
+
+  # Configure and build the project
   cmake -S . -B build/linux/arm64/$build_type \
     -G "Unix Makefiles" \
     -DCMAKE_BUILD_TYPE=$build_type \
     -DRC2D_ARCH=arm64
   cmake --build build/linux/arm64/$build_type
+
+  # Generate AppImage
+  echo -e "\e[32m\nGenerating AppImage ($build_type)...\e[0m"
+  cmake --install "$build_dir" --component Runtime
 done
 
 # Final message
