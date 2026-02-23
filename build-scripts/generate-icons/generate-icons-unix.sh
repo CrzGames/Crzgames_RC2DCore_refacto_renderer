22e205319d49cb98d5ac9259ca6a8edb1cc40f4a
Corentin Recanzone <corentindevpro@hotmail.com>
2026-02-23 17:54:30 +0100

    X

--- build-scripts/generate-icons/generate-icons-unix.sh
+++ build-scripts/generate-icons/generate-icons-unix.sh
@@ -22,19 +22,14 @@ cargo tauri icon --output icons/outputs icons/app-icon-default.png
 # Renommer et copier les fichiers icônes dans leurs dossiers respectifs
 # Windows
 cp icons/outputs/icon.ico icons/windows/app-icon.ico 2>/dev/null || echo "icon.ico non trouvé."
-# macOS
+# macOS / iOS
 cp icons/outputs/icon.icns icons/macos/app-icon.icns 2>/dev/null || echo "icon.icns non trouvé."
 # Linux
 cp icons/outputs/32x32.png icons/linux/app-icon-32x32.png 2>/dev/null || echo "32x32.png non trouvé."
 cp icons/outputs/128x128.png icons/linux/app-icon-128x128.png 2>/dev/null || echo "128x128.png non trouvé."
 cp icons/outputs/128x128@2x.png icons/linux/app-icon-256x256.png 2>/dev/null || echo "128x128@2x.png non trouvé."
 cp icons/outputs/icon.png icons/linux/app-icon.png 2>/dev/null || echo "icon.png non trouvé."
+# Android
+cp -r icons/outputs/android/* android-project/app/src/main/res/ 2>/dev/null || echo "Icônes Android non trouvées."
 
-echo -e "\e[32m Icons Windows, macOS, Linux generer avec succès !\e[0m"
-
-cd ../
-rm -rf outputs
-
-# Copier les icônes Android vers le répertoire de destination du projet Android.
-cp -r android/* ../android-project/app/src/main/res/
-echo -e "\e[32m Icons Android copiés avec succès vers : android-project/app/src/main/res/\e[0m"
+echo -e "\e[32m Icons Windows, macOS, iOS et Android generer avec succès !\e[0m"
